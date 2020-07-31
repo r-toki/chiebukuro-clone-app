a3cabb038aa1f562675dfb33e09c43188656ef64
ReiSogawa <natto.and.eggs@gmail.com>
2020-07-31 21:35:31 +0900

    add signup/out functions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'invalid name/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  resources :users
+
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root to: 'static_pages#home'
+
+  get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  delete '/signout', to: 'sessions#destroy'
+
 end
