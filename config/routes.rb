5d14d2f9168e655858785371d86e6e3e617c1efc
ReiSogawa <natto.and.eggs@gmail.com>
2020-07-31 23:22:04 +0900

    modify route.rb a bit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create, :show]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:create, :show]
+  # 'users#new' は get '/singup' と対応させるから必要ない？
 
   root to: 'static_pages#home'
 
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
 
 end
---
+++ doc/thoughts.md
@@ -0,0 +1,6 @@
+# 開発中の感想をただ書き連ねる
+
+- sessions を singular にしたい。
+  - resources を使わずに個別にルートを書く？
+
+- signup > user > signup の手順で一瞬flashが表示されるのはバグ？
