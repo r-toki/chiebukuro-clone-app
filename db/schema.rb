da8053dc4e1eea2b8bee742bee7a4d7f956a4994
ReiSogawa <natto.and.eggs@gmail.com>
2020-07-31 23:38:28 +0900

    add timestamps to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_142641) do
+ActiveRecord::Schema.define(version: 2020_07_31_143324) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
