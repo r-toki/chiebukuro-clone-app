945578e684ca47b48352772b345ca812017b4851
ReiSogawa <natto.and.eggs@gmail.com>
2020-08-08 11:58:15 +0900

    UPDATE: User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+
+  has_secure_password
+  # 1. password, password_confirmation 属性の追加
+  # 2. authenticate method の追加
+  # 3. validation の追加
+  #   1. create 時 password が存在すること
+  #   2. password の長さが 72 byte 以下であること
+  #   3. password == password_confirmation であること
+
+  validates :name, presence: true,
+                   uniquness: true
+
+  validates :password, presence: true,
+                       allow_nil: true,
+                       length: { minimum: 6 }
+
 end
---
+++ doc/user_related_stuff.md
@@ -0,0 +1,8 @@
+## 変更点
+
+### user
+
+- model へ has_secure_password を追加
+- model へ validation を追加
+- sign up 機能
+- db 側でも name に対してユニーク制約を課す
