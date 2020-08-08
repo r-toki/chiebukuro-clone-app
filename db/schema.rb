42760e0c6c46e9478421bb06305d71d6cbeab2ae
ReiSogawa <natto.and.eggs@gmail.com>
2020-08-08 14:39:54 +0900

    RAILS G: scaffold Question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_031646) do
+ActiveRecord::Schema.define(version: 2020_08_08_052311) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.boolean "is_resolved", default: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -20,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_08_08_031646) do
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end
---
+++ doc/feature__question_related_stuff.md
@@ -0,0 +1,7 @@
+## 変更点
+
+### question
+
+- rails g scaffold Question title:string content:text is_resolved:boolean user:references -> done
+- 生成された db migration の変更と実行 -> done
+  - user_id and created_at に対して index は追加しない
