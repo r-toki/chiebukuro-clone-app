8604f5619b44ddcff1706def064026d26a40d0e7
Rei-Sogawa <52219641+Rei-Sogawa@users.noreply.github.com>
2020-08-10 22:52:54 +0900

    Revert "Feature/answer"

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
 Rails.application.routes.draw do
-
-  root 'static_pages#home'
-
-  get '/signup', to: 'users#new'
-  get '/login', to: 'session#new'
-  post '/login', to: 'session#create'
-  delete '/logout', to: 'session#destroy'
-
-  post '/static_pages/create_question', to: 'static_pages#create_question'
-
-  resources :users
-  resources :questions
-  resources :answers
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200808031646_change_users.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUsers < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :users, :name, false
-    change_column_null :users, :password_digest, false
-  end
-end
