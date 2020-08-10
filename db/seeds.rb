3da0f3c2e0d854eb1c546638b504eedae0eb53e1
ReiSogawa <natto.and.eggs@gmail.com>
2020-08-10 13:34:44 +0900

    seed.rb を調整する

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,29 @@
-users = ["Nao", "Megu", "Kaede"]
-questions =  File.read(Rails.root.join('db', 'random_questions.txt')).split("\n")
+time_now  = Time.now
 
-users.each do |user|
+user_names = ["Nao", "Megu", "Kaede"]
+question_titles =  File.read(Rails.root.join('db', 'random_questions.txt')).split("\n")
+
+user_names.each do |name|
+  created_at = Time.now - (3*365).days
   User.create!(
-    name: user,
+    name: name,
     password: "password",
-    password_confirmation: "password"
+    password_confirmation: "password",
+    created_at: created_at,
+    updated_at: created_at
   )
 end
 
-questions.each do |question|
+question_titles.each do |title|
+  user_id = 1 + rand(user_names.length)
+  user = User.find_by_id(user_id)
+  created_at = rand(user.created_at..time_now)
   Question.create!(
-    title: question,
+    title: title,
     content: Faker::Lorem.sentence(word_count: 20),
     is_resolved: [true, false].sample,
-    user_id: 1 + rand(users.length)
+    user_id: user_id,
+    created_at: created_at,
+    updated_at: created_at
   )
 end
