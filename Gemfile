e6cecc1bc7221185ebb1aa615f966600c3786492
ReiSogawa <natto.and.eggs@gmail.com>
2020-08-23 23:47:18 +0900

    ADD: gredel

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.8'
 
 gem 'haml-rails'
 gem 'will_paginate-bootstrap4'
+gem 'gretel'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
---
+++ config/breadcrumbs.rb
@@ -0,0 +1,20 @@
+crumb :root do
+  link "Home", root_path
+end
+
+crumb :resolved_questions do
+  link "Resolved Questions", questions_path(resolved: true)
+end
+
+crumb :unresolved_questions do
+  link "Unresolved Questions", questions_path(resolved: false)
+end
+
+crumb :question do |question|
+  link question.title, question
+  if question.resolved
+    parent :resolved_questions
+  else
+    parent :unresolved_questions
+  end
+end
